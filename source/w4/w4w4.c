f9f74183a2df41e7f1eaafe7532d2872c1bb6eb5
Filieka <lol.chend6@gmail.com>
2023-10-16 01:56:08 -0700

    update

---
+++ source/w4/w4w4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    char num[1001];    
+    while(1){
+        int sum1=0,sum2=0,i;
+        scanf("%s", num);
+        if (num[0]=='0'){
+            break;
+        }
+        for(i=0;num[i]!='\0';i++){
+            if (i%2==0) {
+                sum1+=num[i]-'0';
+            } else {
+                sum2+=num[i]-'0';
+            }
+        }
+        int d=sum1-sum2;        
+        if(d%11==0){
+            printf("%s is a multiple of 11.\n", num);
+        } else {
+            printf("%s is not a multiple of 11.\n", num);
+        }
+    }
+
+    return 0;
+}
+
