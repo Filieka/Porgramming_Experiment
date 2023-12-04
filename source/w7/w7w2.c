8ac24aac4ba881935ff16dd28431a8de9e3f8718
Filieka <lol.chend6@gmail.com>
2023-12-04 00:59:24 -0800

    Update w7w2.c

--- source/w7/w7w2.c
+++ source/w7/w7w2.c
@@ -7,22 +7,58 @@ int search(int goal,int* ans){
     for(i=0;ans[i]!=goal;i++){}
     return i;
 }
+void inital(int location,int num,int* ans,int max){
+    int i=0,count=0;
+    while(1){
+        if(ans[i]==-1){
+            count++;
+            if(count>num){
+                break;
+            }
+        }
+        i++;
+    }
+    memmove(&ans[location],&ans[location+1],(max-location-1)*sizeof(int));
+    if(i>location){
+        i--;
+    }
+    memmove(&ans[i+1],&ans[i],(max-i-1)*sizeof(int));
+    ans[i]=num;
+}
 void moveonto(int num1,int num2,int* ans,int max){
     int num1_location=search(num1,ans);
-    int snap=ans[num1_location];
     int i;
+    if(ans[num1_location+1]!=-1){
+        for(i=num1_location+1;ans[i]!=-1;i++){
+            inital(i,ans[i],ans,max);
+            i=search(num1,ans);
+        }
+    }
+    int snap=ans[num1_location];
     if(num1==num2){
         return;
     }
     memmove(&ans[num1_location],&ans[num1_location+1],(max-num1_location-1)*sizeof(int));
     int num2_location=search(num2,ans);
+    if(ans[num2_location+1]!=-1){
+        for(i=num2_location+1;ans[i]!=-1;i++){
+            inital(i,ans[i],ans,max);
+            i=search(num2,ans);
+        }
+    }
     memmove(&ans[num2_location+2],&ans[num2_location+1],(max-num2_location-2)*sizeof(int));
     memcpy(&ans[num2_location+1],&snap,1*sizeof(int));
 }
 void moveover(int num1,int num2,int* ans,int max){
     int num1_location=search(num1,ans);
-    int snap=ans[num1_location];
     int i;
+    if(ans[num1_location+1]!=-1){
+        for(i=num1_location+1;ans[i]!=-1;i++){
+            inital(i,ans[i],ans,max);
+            i=search(num1,ans);
+        }
+    }
+    int snap=ans[num1_location];
     if(num1==num2){
         for(i=num1_location;ans[i]!=-1;i++){}
         memmove(&ans[num1_location],&ans[num1_location+1],(i-num1_location-1)*sizeof(int));
@@ -36,8 +72,15 @@ void moveover(int num1,int num2,int* ans,int max){
     memcpy(&ans[i],&snap,1*sizeof(int));
 }
 void pileonto(int num1,int num2,int* ans,int max){
-    int num1_location=search(num1,ans);
+    int num2_location=search(num2,ans);
     int i,j;
+    if(ans[num2_location+1]!=-1){
+        for(i=num2_location+1;ans[i]!=-1;i++){
+            inital(i,ans[i],ans,max);
+            i=search(num2,ans);
+        }
+    }
+    int num1_location=search(num1,ans);
     for(i=num1_location;ans[i]!=-1;i++){}
     int* snap=(int*)malloc((i-num1_location)*sizeof(int));
     memcpy(snap,&ans[num1_location],(i-num1_location)*sizeof(int));
@@ -48,7 +91,7 @@ void pileonto(int num1,int num2,int* ans,int max){
         }
     }
     memmove(&ans[num1_location],&ans[i],(max-i)*sizeof(int));
-    int num2_location=search(num2,ans);
+    num2_location=search(num2,ans);
     memmove(&ans[num2_location+i-num1_location+1],&ans[num2_location+1],(max-num2_location-1-i+num1_location)*sizeof(int));
     memcpy(&ans[num2_location+1],snap,(i-num1_location)*sizeof(int));
     free(snap);
