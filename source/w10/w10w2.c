b9a60ec9ceaf5a003e5cbef427db86cd380bde62
Filieka <144932126+Filieka@users.noreply.github.com>
2023-12-20 11:02:01 +0800

    Create w10w2.c

---
+++ source/w10/w10w2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+#define sl (line.point.y-point.y)/(line.point.x-point.x)
+
+typedef struct {
+  float x, y;
+} Point;
+
+typedef struct {
+  Point point;
+  float slope;
+} Line;
+
+int main(){
+    Line line;
+    Point point;
+    float s;
+    scanf("%f%*c%f%*c%f",&line.point.x,&line.point.y,&line.slope);
+    scanf("%f%*c%f",&point.x,&point.y);
+    s=(float)fabs(sl);
+    if(s==line.slope){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+    
+  
+}
